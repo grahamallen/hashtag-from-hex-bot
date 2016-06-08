84d8447454fd2667dc1dfcf46265d98bca4adad8
grahamallen <g.auerbach.allen@gmail.com>
2016-06-07 17:27:12 -0700

    Successfully streams and replies without pics

--- bin/run.js
+++ bin/run.js
@@ -1,3 +1,15 @@
 var hashHex = require('..')
 
-hashHex.readAllTweets().then(console.log)
+hashHex.readTweets()
+hashHex.replyToTweets()
+
+function debug(id) {
+  hashHex.T.get('statuses/show/' + id, function(err, data, response) {
+    if (err) { console.log(err) }
+    hashHex.processTweet(data).then(function(processedMedia) {
+      var metaData = processedMedia.shift()
+      hashHex.replyQueue.push(metaData)
+      hashHex.replyToTweet()
+    }).catch(console.error)
+  })
+}
